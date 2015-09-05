ab297ce521bc29ec660f0a87f3332bce528c35b1
uhura <uhura@Uhuras-Mac-mini.local>
2015-09-05 16:00:13 +0700

    captcha

---
+++ captcha.hs
@@ -0,0 +1,7 @@
+number = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+operation = ["","+","*","-"]
+
+captcha 1 st oper nd = number!!st ++ " " ++ operation!!oper ++ " " ++ show nd
+captcha 2 st oper nd = show st ++ " " ++ operation!!oper ++ " " ++ number!!nd
+captcha 3 st oper nd = number!!st ++ " " ++ operation!!oper ++ " " ++ number!!nd
+captcha 4 st oper nd = show st ++ " " ++ operation!!oper ++ " " ++ show nd
